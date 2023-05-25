3c58432a248014764064b9fd6a07516f3bec848d
apop274 <112342983+apop274@users.noreply.github.com>
2023-05-24 21:00:20 -0700

    Update barcelona.java

--- FootballStatistics/src/mainpkg/barcelona.java
+++ FootballStatistics/src/mainpkg/barcelona.java
@@ -1,13 +1,22 @@
-package mainpkg;
+package mainPkg;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Panel;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 public class barcelona implements ActionListener{
 	
@@ -65,6 +74,12 @@ public class barcelona implements ActionListener{
 		teamPanel.setLayout(null);
 		teamPanel.setBackground(Color.blue);
 		
+		ImageIcon barca_img = new ImageIcon(getClass().getResource("/mainPkg/barcaLogo.png"));
+		JLabel barcaImgLabel = new JLabel(barca_img);
+		barcaImgLabel.setBounds(100, 150, 297, 300);
+		teamPanel.add(barcaImgLabel);
+		
+		
 		JButton barcaButton = new JButton("F.C. Barcelona");
 		barcaButton.setBounds(12, 500, 200, 25);
 		teamPanel.add(barcaButton);
@@ -84,11 +99,19 @@ public class barcelona implements ActionListener{
 				barcaPanel.setLayout(null);
 				barcaPanel.setBackground(barcaPanelColor);
 				
+				ImageIcon img = new ImageIcon(getClass().getResource("/mainPkg/barcaLogo.png"));
+				JLabel imgLabel = new JLabel(img);
+				imgLabel.setBounds(100, 150, 297, 300);
+				barcaPanel.add(imgLabel);
+				
+				JLabel barcaLogoText = new JLabel("F.C. Barcelona");
+				barcaLogoText.setBounds(183, 340, 300, 300);
+				barcaLogoText.setFont(new Font("Serif Bold", Font.BOLD, 20));
+				barcaPanel.add(barcaLogoText);
+				
 				barcaFrame.setVisible(true);
 				teamFrame.setVisible(false);
 				frame.setVisible(false);
-				
-				
 				
 			}
 		});
@@ -114,8 +137,12 @@ public class barcelona implements ActionListener{
 				mancityPanel.setLayout(null);
 				mancityPanel.setBackground(mancityPanelColor);
 				
-				
 				mancityFrame.setVisible(true);
+				Rectangle rect = new Rectangle(10, 10, 10, 10);
+
+				
+				
+				
 				frame.setVisible(false);
 				teamFrame.setVisible(false);
 			}
