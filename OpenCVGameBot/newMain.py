0b6dc3b72e3d30b83e4fbeac54a6c15e4a71dfe1
apop274 <112342983+apop274@users.noreply.github.com>
2023-05-30 12:13:01 -0700

    Update newMain.py

--- OpenCVGameBot/newMain.py
+++ OpenCVGameBot/newMain.py
@@ -2,11 +2,14 @@ import cv2
 from PIL import ImageGrab
 import numpy as np
 from PIL.Image import NONE
+import pyautogui
 
-top_leftx = 360
-top_lefty = 85
-bottom_rightx = 575
-bottom_righty = 210
+top_leftx = 620
+top_lefty = 120
+bottom_rightx = 1300
+bottom_righty = 650
+
+x, y = pyautogui.position()
 
 def grabScreen(bbox = None):
  img = ImageGrab.grab(bbox = bbox)
@@ -14,6 +17,9 @@ def grabScreen(bbox = None):
  img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
  return img
 
+while 1:
+ pyautogui.click(x, y)
+ 
 while 1:
  img = grabScreen(bbox=(top_leftx, top_lefty, bottom_rightx, bottom_righty))
 
