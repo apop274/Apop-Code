3bb7678f441d9d79da49d0c751e6a32b87639a90
apop274 <112342983+apop274@users.noreply.github.com>
2023-07-11 11:44:30 -0700

    Update binaryConverter.py

--- Binary Converter/binaryConverter.py
+++ Binary Converter/binaryConverter.py
@@ -1,34 +1,18 @@
-user_input = input("hello, what number would you like?: ")
-num = int(user_input)
+def int_to_bin(integer):
+    if integer == 0:
+        return 0
 
-returnVal = ""
-counter = 0
-value = 0
+    binary = 0
+    digit_place = 1
 
+    while integer > 0:
+        remainder = integer % 2
+        binary += remainder * digit_place
+        integer = integer // 2
+        digit_place *= 10
 
-while value < num:
-  
- value = 2**counter
+    return binary
 
- counter += 1
-
-
-counter -= 1
-
-newVal = 0
-
-while counter > -1:
- newVal = 2**counter
-
- num -= newVal
-
- if num < 0:
-  returnVal = returnVal + "0"
-  num += newVal
- else:
-  returnVal = returnVal + "1"
-
- counter -= 1
-
-returnVal = returnVal[1:]
-print(returnVal)
+integer_number = int(input("Enter an integer: "))
+binary_number = int_to_bin(integer_number)
+print("Binary representation:", binary_number)
