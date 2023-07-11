0fde98ce0119cb65f92be58211e61218ebfaff8a
apop274 <112342983+apop274@users.noreply.github.com>
2023-07-11 11:43:09 -0700

    Update gui.py

--- Binary Converter/gui.py
+++ Binary Converter/gui.py
@@ -1,78 +1,41 @@
-#tkinter heirarchy: root window --> frame --> widgets
-
 import tkinter as tk
-import tkinter.messagebox
-from tkinter import *
+
+def int_to_bin():
+    integer=int(entry.get())
+
+    if integer == 0:
+        return 0
+
+    binary = 0
+    digit_place = 1
+
+    while integer > 0:
+        remainder = integer % 2
+        binary += remainder * digit_place
+        integer = integer // 2
+        digit_place *= 10
+
+   # return binary
+    binaryResultLabel.config(text= binary)
+    
 
 
-def getInput():
-   #input = textbox.get("1.0", 'end-1c')   
-   input= tk.Entry.get()
-
-
-def displayOutput():
- 
- input = 0 
- getInput()
- 
- returnVal = ""
- counter = 0
- value = 0
-
-
- while value < input:
-  value = 2**counter
-
-
- counter -= 1
-
- newVal = 0
-
- while counter > -1:
-  newVal = 2**counter
-
-  input -= newVal
-
-  if input < 0:
-   returnVal = returnVal + "0"
-   input += newVal
-  else:
-   returnVal = returnVal + "1"
-
-  counter -= 1
-
- returnVal = returnVal[1:]
- #print(returnVal)
-
- #tkinter.messagebox.showinfo("Binary Converted Result", returnVal)
- print(returnVal)
-
-
-
-#root window
+#main window
 window = tk.Tk()
-window.title("Binary Converter")
-window.geometry('1000x600')
+window.title("Integer to Binary Converter")
+window.geometry("600x300")
 
+#entryfield
 entry = tk.Entry(window)
-entry.pack()
+entry.pack(pady=30)
+
+#button
+button = tk.Button(window, text="Convert", command=int_to_bin, font = ('Arial', 18))
+button.pack(pady=0)
+
+#result displaying label
+binaryResultLabel = tk.Label(window, text="Binary Version: ")
+binaryResultLabel.pack(pady=25)
 
 
-label = tk.Label(window, text="Integer to Binary Converter", font = ('Arial', 45))
-label.pack(padx=100, pady = 40)
-
-textbox = tk.Text(window, height=2, font= ('Arial',16))
-textbox.pack(padx=300)
-
-button = tk.Button(window, text="Convert", command = displayOutput)   # font = ('Arial', 13)
-button.pack(padx=0, pady=10)
-
-label = tk.Label(window, text= "Binary Value: ",  font=('Arial', 18))
-label.pack(pady=50)
-
-#returnBox = tk.Text(window, height = 2, font = ('Arial', 16))
-#returnBox.pack(padx = 100, pady=0)
-
-#run the main event loop
 window.mainloop()
-
