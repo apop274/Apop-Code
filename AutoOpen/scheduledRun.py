8748fadb8aa4944d338ded257067d458ec97d889
apop274 <112342983+apop274@users.noreply.github.com>
2023-12-10 16:19:59 -0800

    Update scheduledRun.py

--- AutoOpen/scheduledRun.py
+++ AutoOpen/scheduledRun.py
@@ -1,3 +1,5 @@
+#this program opens lolMiner at the sunny times, for the mining to occur at the sunny times
+
 import subprocess
 import schedule
 import time
@@ -5,24 +7,34 @@ import os
 
 def open_app():
    
-    app_path = r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk'
-    subprocess.Popen([app_path], shell=True)
+   # app_path = r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk'
+    app_path = r'C:\Users\Xxapo\OneDrive\Desktop\Nexa coin\lolMiner_v1.77b_Win64\1.77b\mine_nexa'
+    subprocess.Popen(['start', 'cmd', '/c', app_path], shell=True)
 
 def close_app():
     
-    app_name = 'chrome.exe'
-    os.system(f'taskkill /f /im {app_name}')
+    #app_name = 'chrome.exe'
+    #app_name = 'cmd.exe'
+    #app_name = 'lolMiner.exe'
+    subprocess.Popen(['taskkill', '/f', '/im', 'lolMiner.exe'], shell=True)
+    subprocess.Popen(['taskkill', '/f', '/im', 'cmd.exe'], shell=True)
+    #os.system(f'taskkill /f /im {app_name}')
 
 #open time
-schedule.every().day.at("21:41").do(open_app)
+schedule.every().day.at("7:15").do(open_app)
 
 #close time
-schedule.every().day.at("21:42").do(close_app)
+schedule.every().day.at("16:30").do(close_app)
 
-#schedule.every().day.at("16:54").do(open_app)
-#schedule.every().day.at("16:55").do(close_app)
+#for testing
+#schedule.every().day.at("16:12").do(open_app)
+#schedule.every().day.at("16:14").do(close_app)
+
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-    print("working...")
+    print('running...')
+
+
+#to close: click in terminal, presss ctrl+c
